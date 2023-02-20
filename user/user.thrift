01841a60e8f6b272a86a9f20a4df7164a2841c2d
composer <2789706336@qq.com>
2023-02-20 11:20:50 +0800

    feat: idl update

--- user/user.thrift
+++ user/user.thrift
@@ -6,8 +6,17 @@ struct UserInfoResponse {
     1: required response.Response response
     2: i64 id
     3: string name
+    4: i64 followCount
+    5: i64 followerCount
+    6: bool isFollow
+    7: string avatar
+    8: string backgroundImage
+    9: string signature
+    10: i64 totalFavorited
+    11: i64 workCount
+    12: i64 favoriteCount
 }
 
 service User {
-    UserInfoResponse userInfo(1: i64 userId)
+    UserInfoResponse userInfo(1: i64 userId, 2: i64 queryId)
 }
